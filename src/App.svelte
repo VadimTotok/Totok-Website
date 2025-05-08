761708ddd78f978e81a783cad94b3d9f5201f371
Vadim Totok <112573798+VadimTotok@users.noreply.github.com>
2025-05-08 11:51:07 +0300

    1

---
+++ src/App.svelte
@@ -0,0 +1,42 @@
+<script lang="ts"></script>
+
+<main>
+  <div class="page-container">
+    <div class="profile-card">
+      <img src="./public/avatar.jpeg" alt="Vadim Totok photo" class="photo">
+      <h1>
+        Hi, I'm
+        <span class="name">Vadim Totok</span>
+      </h1>
+      <h1>Software developer</h1>
+      <p class="description">Developer who fiddles with all sorts of stuff. Linux enjoyer. Guitar player and... I ABSOLUTELY LOVE COOKING!</p>
+      <div class="contact">
+        <a href="https://github.com/VadimTotok" target="_blank">
+          <img src="./public/contact/github.svg" alt="github">
+        </a>
+        <a href="https://discord.com/users/1368525716840251515" target="_blank">
+          <img src="./public/contact/discord.svg" alt="discord">
+        </a>
+        <a href="mailto:vadimtotok.dev@gmail.com">
+          <img src="./public/contact/gmail.svg" alt="gmail">
+        </a>
+      </div>
+      <div class="stack">
+        <h1>Stack</h1>
+        <img src="./public/stack/Java.png" alt="Java">
+        <img src="./public/stack/Kotlin.png" alt="Kotlin">
+        <img src="./public/stack/Python.png" alt="Python">
+        <img src="./public/stack/TypeScript.png" alt="TypeScript">
+        <img src="./public/stack/PostgresSQL.png" alt="PostgreSQL">
+        <img src="./public/stack/Docker.png" alt="Docker">
+        <h1>Tools</h1>
+        <img src="./public/stack/idea.png" alt="IntellijIdea">
+        <img src="./public/stack/vscode.png" alt="Visual Studio Code">
+        <img src="./public/stack/Figma.png" alt="Figma">
+        <img src="./public/stack/Obsidian.png" alt="Obsidian">
+      </div>
+    </div>
+  </div>
+</main>
+
+<style></style>
